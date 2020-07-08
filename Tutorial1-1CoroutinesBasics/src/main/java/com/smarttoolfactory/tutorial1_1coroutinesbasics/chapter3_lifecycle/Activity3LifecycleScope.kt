3e17abd9ab39dbe144982ff4b1cef622f3792e33
Omer Iyioz <omeriyioz@gmail.com>
2020-07-08 21:00:42 +0300

    added comments

--- Tutorial1-1CoroutinesBasics/src/main/java/com/smarttoolfactory/tutorial1_1coroutinesbasics/chapter3_lifecycle/Activity3LifecycleScope.kt
+++ Tutorial1-1CoroutinesBasics/src/main/java/com/smarttoolfactory/tutorial1_1coroutinesbasics/chapter3_lifecycle/Activity3LifecycleScope.kt
@@ -12,7 +12,40 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /*
-*
+    lifecycleScope.launch{} is an alternatie to Handler().postDelayed()
+
+    activity.lifecycleScope.launch {
+     // scope bound to Activity Lifecycle
+    }
+    fragment.lifecycleScope.launch {
+     // scope bound to Fragment Lifecycle
+    }
+    fragment.viewLifecycleOwner.launch{
+     // scope bound to Fragment View
+    }
+
+    Be aware that lifecycleScope is convenient when dealing with UI events like, for example, showing a tip for the user and hiding it after a small delay.
+    lifecycleScope.launch {
+        delay(5000)
+        showTip()
+        delay(5000)
+        hideTip()
+    }
+
+    Without using Coroutines and lifecycleScope, this would be:
+    val DELAY = 5000
+    Handler().postDelayed({
+      showTip()
+        Handler().postDelayed({
+          hideTip()
+        }, DELAY)
+    }, DELAY)
+
+    LifecycleScope is bound to Dispatcher.Main. That means if you don’t change Dispatcher explicitly all the coroutines inside LifecycleScope will be executed on the main thread.
+
+    https://medium.com/corouteam/exploring-kotlin-coroutines-and-lifecycle-architectural-components-integration-on-android-c63bb8a9156f
+    https://android.jlelse.eu/coroutine-in-android-working-with-lifecycle-fc9c1a31e5f3
+    https://kotlin.christmas/2019/13
 * */
 
 class Activity3LifecycleScope : AppCompatActivity(R.layout.activity3_lifecycle_scope) {
